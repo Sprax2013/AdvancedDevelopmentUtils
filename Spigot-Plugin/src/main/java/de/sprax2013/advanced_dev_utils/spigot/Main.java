9010c92f26efcb715bbbc72d365ff08c5bc5045b
Sprax2013 <Developer@sprax2013.de>
2019-09-21 13:58:11 +0200

    Added bStats + Code reformat

--- Spigot-Plugin/src/main/java/de/sprax2013/advanced_dev_utils/spigot/Main.java
+++ Spigot-Plugin/src/main/java/de/sprax2013/advanced_dev_utils/spigot/Main.java
@@ -1,70 +1,72 @@
 package de.sprax2013.advanced_dev_utils.spigot;
 
-import java.io.FileWriter;
-import java.util.Arrays;
-import java.util.List;
-
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import de.sprax2013.advanced_dev_utils.spigot.events.CustomEventManager;
 import de.sprax2013.advanced_dev_utils.spigot.fake_player.FakePlayerManager;
 import de.sprax2013.advanced_dev_utils.spigot.utils.BukkitServerUtils;
 import de.sprax2013.advanced_dev_utils.spigot.utils.JavaPluginUtils;
 import de.sprax2013.advanced_dev_utils.spigot.utils.plugin_messaging.PluginMessagingChannelUtils;
+import org.bstats.bukkit.Metrics;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main extends JavaPlugin {
-	private static Main plugin;
+    private static Main plugin;
 
-	private final List<String> optionalDepends = Arrays.asList();
+    private final List<String> optionalDepends = Arrays.asList();
 
-	public static final String prefix = "§eAdvancedDevUtils-Spigot §8» §r";
+    public static final String prefix = "§eAdvancedDevUtils-Spigot §8» §r";
 
-	FileWriter fW;
+    FileWriter fW;
 
-	@Override
-	public void onEnable() {
-		plugin = this;
+    @Override
+    public void onEnable() {
+        plugin = this;
 
-		Bukkit.getConsoleSender()
-				.sendMessage(Main.prefix + "§2Erkannte Server-Version§7: §e" + BukkitServerUtils.getBukkitVersion());
+        Bukkit.getConsoleSender()
+                .sendMessage(Main.prefix + "§2Erkannte Server-Version§7: §e" + BukkitServerUtils.getBukkitVersion());
 
-		if (!JavaPluginUtils.checkDependencies(this, prefix, optionalDepends)) {
-			Bukkit.getPluginManager().disablePlugin(this);
-		} else {
-			de.sprax2013.advanced_dev_utils.Main.init();
+        if (!JavaPluginUtils.checkDependencies(this, prefix, optionalDepends)) {
+            Bukkit.getPluginManager().disablePlugin(this);
+        } else {
+            de.sprax2013.advanced_dev_utils.Main.init();
 
-			CustomEventManager.init();
-			PluginMessagingChannelUtils.init();
+            CustomEventManager.init();
+            PluginMessagingChannelUtils.init();
 
-			// Create Files
+            // Create Files
 //			getYAMLConfig();
 
-			// Init plugin
+            // Init plugin
 //			ClientSettingsAPI.init();
 
-			registerListeners();
+            registerListeners();
 
-			// DEBUG
-			FakePlayerManager.debug();
-		}
-	}
+            // DEBUG
+            FakePlayerManager.debug();
+        }
 
-	@Override
-	public void onDisable() {
-		de.sprax2013.advanced_dev_utils.Main.deInit();
-	}
+        Metrics bStats = new Metrics(this);
+    }
 
-	private void registerListeners() {
-		PluginManager pm = Bukkit.getPluginManager();
+    @Override
+    public void onDisable() {
+        de.sprax2013.advanced_dev_utils.Main.deInit();
+    }
 
-		pm.registerEvents(new CustomEventManager(), this);
-	}
+    private void registerListeners() {
+        PluginManager pm = Bukkit.getPluginManager();
 
-	public static Main getInstance() {
-		return plugin;
-	}
+        pm.registerEvents(new CustomEventManager(), this);
+    }
+
+    public static Main getInstance() {
+        return plugin;
+    }
 
 //	public static FileConfiguration getYAMLConfig() {
 //		YAMLFile file = YAMLFileManager.getFile(getInstance(), "config.yml");
