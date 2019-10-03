25c78edc4765d45de186a285e04a1f179db8bf02
Sprax2013 <Developer@sprax2013.de>
2019-10-03 22:27:33 +0200

    Made ActionBarAPI compatible with v1_12_R1

--- Spigot-Plugin/src/main/java/de/sprax2013/advanced_dev_utils/spigot/chat/actionbar/ActionBarAPI.java
+++ Spigot-Plugin/src/main/java/de/sprax2013/advanced_dev_utils/spigot/chat/actionbar/ActionBarAPI.java
@@ -1,35 +1,36 @@
 package de.sprax2013.advanced_dev_utils.spigot.chat.actionbar;
 
+import de.sprax2013.advanced_dev_utils.spigot.nms.chat.actionbar.v1_12_R1;
+import de.sprax2013.advanced_dev_utils.spigot.utils.BukkitServerUtils;
+import de.sprax2013.advanced_dev_utils.spigot.utils.MCPacketUtils;
 import org.bukkit.entity.Player;
 
-import de.sprax2013.advanced_dev_utils.spigot.utils.BukkitServerUtils;
-
 public class ActionBarAPI {
-	/**
-	 * Displays an ActionBar to a player using NMS.
-	 *
-	 * @param p The Player
-	 * @param s The String to display
-	 *
-	 * @return false, if Server-Version not supported
-	 */
-	public static boolean sendActionBar(Player p, String s) {
-		switch (BukkitServerUtils.getBukkitVersion()) {
-		case "v1_8_R3":
-			v1_8_R3.sendActionBar(p, s);
-			break;
-//		case "v1_12_R1":
-//			v1_12_R1.sendActionBar(p, s);
-//			break;
+    /**
+     * Displays an ActionBar to a player using NMS.
+     *
+     * @param p The Player
+     * @param s The String to display
+     *
+     * @return false, if Server-Version not supported
+     */
+    public static boolean sendActionBar(Player p, String s) {
+        switch (BukkitServerUtils.getBukkitVersion()) {
+            case "v1_8_R3":
+                MCPacketUtils.sendPacket(p, v1_8_R3.createActionBarPacket(s));
+                break;
+            case "v1_12_R1":
+                MCPacketUtils.sendPacket(p, v1_12_R1.createActionBarPacket(s));
+                break;
 //		case "v1_13_R2":
 //			v1_13_R2.sendActionBar(p, s);
 //			break;
-		default:
-			System.err.println("[!! WARNING !!] ActionBarAPI does not support Server-Version "
-					+ BukkitServerUtils.getBukkitVersion());
-			return false;
-		}
+            default:
+                System.err.println("[!! WARNING !!] ActionBarAPI does not support Server-Version "
+                        + BukkitServerUtils.getBukkitVersion());
+                return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 }
--- Spigot-Plugin/src/main/java/de/sprax2013/advanced_dev_utils/spigot/chat/actionbar/v1_8_R3.java
+++ Spigot-Plugin/src/main/java/de/sprax2013/advanced_dev_utils/spigot/chat/actionbar/v1_8_R3.java
@@ -1,16 +1,14 @@
 package de.sprax2013.advanced_dev_utils.spigot.chat.actionbar;
 
-import org.bukkit.entity.Player;
-
-import de.sprax2013.advanced_dev_utils.spigot.utils.MCPacketUtils;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
 import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
+import org.bukkit.entity.Player;
 
 public class v1_8_R3 {
-	/**
-	 * @see ActionBarAPI#sendActionBar(Player, String)
-	 */
-	static void sendActionBar(Player p, String s) {
-		MCPacketUtils.sendPacket(p, new PacketPlayOutChat(ChatSerializer.a("{\"text\":\"" + s + "\"}"), (byte) 2));
-	}
+    /**
+     * @see ActionBarAPI#sendActionBar(Player, String)
+     */
+    static Object createActionBarPacket(String s) {
+        return new PacketPlayOutChat(ChatSerializer.a("{\"text\":\"" + s + "\"}"), (byte) 2);
+    }
 }
